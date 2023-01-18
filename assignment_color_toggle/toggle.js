e3dde80e172997366b0f4932ec74591e792169d0
aerdemir75 <67397608+aerdemir75@users.noreply.github.com>
2023-01-18 15:30:26 +0100

    Add files via upload

--- assignment_color_toggle/toggle.js
+++ assignment_color_toggle/toggle.js
@@ -1,48 +1,137 @@
 
 
 
-const buttonGroup = document.getElementById("button-group");
+// const buttonGroup = document.getElementById("button-group");
 
-const buttonGroupPressed = (e) => {
-  const isButton = e.target.nodeName === 'BUTTON';
-  if(!isButton) {
-     return
-   }
-  document.body.style.backgroundColor = e.target.value;
-  e.target.style.backgroundColor =  e.target.value;
+// const buttonGroupPressed = (e) => {
+//   const isButton = e.target.nodeName === 'BUTTON';
+//   if(!isButton) {
+//      return
+//    }
+//   document.body.style.backgroundColor = e.target.value;
+//   e.target.style.backgroundColor =  e.target.value;
   
-}
+// }
 
-buttonGroup.addEventListener('click', buttonGroupPressed);
+// buttonGroup.addEventListener('click', buttonGroupPressed);
 
 
-let solidBar = false;
+// let solidBar = false;
 
-let toggleBar = function() {
-  let getSidebar = document.querySelector('.button-group');
-  let getSidebarButton = document.querySelectorAll('.button-group button');
-  if (solidBar === false) {
+// let toggleBar = function() {
+//   let getSidebar = document.querySelector('.button-group');
+//   let getSidebarButton = document.querySelectorAll('.button-group button');
+//   if (solidBar === false) {
     
-    getSidebar.style.width = "75px";
+//     getSidebar.style.width = "75px";
 
-    let arraylength = getSidebarButton.length;
-    for (let i = 0; i < arraylength; i++) {
-      getSidebarButton[i].style.opacity = "1";
+//     let arraylength = getSidebarButton.length;
+//     for (let i = 0; i < arraylength; i++) {
+//       getSidebarButton[i].style.opacity = "1";
 
-    }
-    solidBar = true;
-  }
-  else if (solidBar === true) {
-    getSidebarButton.style.visibility = "visible";
-    getSidebar.style.width = "75px";
+//     }
+//     solidBar = true;
+//   }
+//   else if (solidBar === true) {
+//     getSidebarButton.style.visibility = "visible";
+//     getSidebar.style.width = "75px";
 
-    let arraylength = getSidebarButton.length;
-    for (let i = 0; i < arraylength; i++) {
-      getSidebarButton[i].style.opacity = "0";
+//     let arraylength = getSidebarButton.length;
+//     for (let i = 0; i < arraylength; i++) {
+//       getSidebarButton[i].style.opacity = "0";
 
-    }
-    getSidebarButton.style.visibility = "hidden";
+//     }
+//     getSidebarButton.style.visibility = "hidden";
     
-    solidBar = false;
-  }
-}
+//     solidBar = false;
+//   }
+// }
+
+
+let isDrunk; // Can be true or false.
+let wearsCoolClothes; // Can be true or false.
+let clubIsFull; // Can be true or false.
+
+// Zafar
+// isDrunk = false;
+// wearsCoolClothes = true;
+// clubIsFull = true;
+
+// Storm
+// isDrunk = true;
+// wearsCoolClothes = true;
+// clubIsFull = true;
+
+
+// Carla
+// isDrunk = false;
+// wearsCoolClothes = false;
+// clubIsFull = true;
+
+// Alice
+// isDrunk = false;
+// wearsCoolClothes = false;
+// clubIsFull = false;
+
+
+// Responses
+// console.log("Please come back when you're sober.");
+// console.log("No, too busy right now.");
+// console.log("Wow, cool clothes, come in!");
+// console.log("Welcome!");
+
+//START CODING HERE!!
+if (isDrunk) {
+  console.log("Please come back when you're sober.");
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
